fd0123be8869a695f3df2bfcd8bfc4ed860c513e
AGRegert <andre.germano.regert@gmail.com>
2016-09-07 21:13:08 -0300

    Project 04
    Machine languages - complementing operations missing in hardware (Mult.asm) and working with input/output (Fill.asm)

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,61 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(MainLoop)
+  // Reading the value from keyboard
+  @KBD
+  D=M
+  
+  // Are we filling the screen? We are if any key is pressed.
+  @ClearScreen
+  D;JEQ
+  @ScreenValue
+  M=-1
+  @StartFilling
+  0;JMP
+
+(ClearScreen)
+  @ScreenValue
+  M=0
+  
+(StartFilling)
+  // Taking the start address
+  @SCREEN
+  D=A
+  @ScreenPosition
+  M=D
+
+  // The screen is 512x256 pixels wide, which 
+  // gives us 8192 words of 16bit registers in memory
+  @8191
+  D=D+A
+  
+  @ScreenEnd
+  M=D
+  
+(FillWord)
+  // getting the selected screen value
+  @ScreenValue
+  D=M
+
+  // Adjusting the screen position 
+  @ScreenPosition
+  A=M
+
+  // Filling the screen
+  M=D
+
+  // Verifying if we reached the end of the screen
+  D=A
+  @ScreenEnd
+  D=M-D
+  @MainLoop // if so, we return to the main loop
+  D;JEQ  
+  
+  // Selecting the next position to write
+  @ScreenPosition
+  M=M+1
+
+  @FillWord
+  0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  // reseting the answer
+  @R2
+  M=0
+
+  // loading the first operator
+  @R0
+  D=M
+
+  // if its zero, just go to the end
+  @END
+  D;JEQ
+
+  // if not, it counts how many times we sum the second operator
+  @times
+  M=D
+  
+  // loading the second operator
+  @R1
+  D=M
+
+  // if its zero, just go to the end
+  @END
+  D;JEQ
+
+  // here we keep its value
+  @value
+  M=D
+
+(LOOP)
+  // adding value one time
+  @value
+  D=M
+  @R2
+  M=D+M
+  
+  // computing how many times we must add the value
+  @times
+  D=M-1
+  M=D
+  @LOOP
+  D;JGT
+
+(END)
+  @END
+  0;JMP
